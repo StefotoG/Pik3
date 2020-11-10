0d5e4f5647cdccf3e96cf04be3ef7e7845aafdaf
StefotoG <71270066+StefotoG@users.noreply.github.com>
2020-11-10 13:34:34 +0200

    added some debug messages

--- Taxi Company/src/com/tusofia/taxi/Main.java
+++ Taxi Company/src/com/tusofia/taxi/Main.java
@@ -2,6 +2,7 @@ package com.tusofia.taxi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -10,9 +11,32 @@ public class Main {
         List<Taxi> taxiList = new ArrayList<Taxi>();
         List<Driver> driverList = new ArrayList<Driver>();
 
-        Driver jigit = new Driver(20,1,1);
-        Taxi tarataika = new Taxi("yellow", "hotHatch");
+        try {
+//            Uncomment to test exception
+//            Driver newDriver = new Driver(20, false, true);
+
+            Driver newDriver = new Driver(20,true,true);
+            System.out.println("New driver was created!\r\n");
+            driverList.add(newDriver);
+            System.out.println("Driver was added to list!\r\n");
+        } catch (DriverException e) {
+            e.printStackTrace();
+        }
+        try {
+//            Uncomment to test exception
+//            Taxi newTaxi = new Taxi("yellow", 1998, "hotHatch");
+
+            Taxi newTaxi = new Taxi("yellow", 2018, "hotHatch");
+            System.out.println("New taxi was created!\r\n");
+            taxiList.add(newTaxi);
+            System.out.println("Taxi was added to list!\r\n");
+        } catch (TaxiException e) {
+            e.printStackTrace();
+        }
+
 
         TaxiCompany company = new TaxiCompany("Maksim", taxiList, driverList);
+        System.out.println("TaxiCompany was created!\r\n");
+
     }
 }
